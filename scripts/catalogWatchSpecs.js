4348c259bf57ff32f4f9558b0707044c3398ea51
Alena Petrych <abelokonskaya.87@gmail.com>
2025-08-29 09:40:23 +0100

    feat: added specs of each watch in catalog page

---
+++ scripts/catalogWatchSpecs.js
@@ -0,0 +1,29 @@
+const watchDeatils = {
+  watch1: {
+    specs: [
+      "Case: Stainless steel",
+      "Strap: Genuine leather",
+      "Water resistance: 50m",
+      "Movement: Quartz",
+      "Glass: Mineral",
+    ],
+  },
+  watch2: {
+    specs: [
+      "Case: Stainless steel",
+      "Strap: Stainless steel",
+      "Water resistance: 50m",
+      "Movement: Quartz",
+      "Glass: Mineral",
+    ],
+  },
+  watch3: {
+    specs: [
+      "Case: Stainless steel",
+      "Strap: Stainless steel",
+      "Water resistance: 100m",
+      "Movement: Mechanical",
+      "Glass: Sapphire crystal",
+    ],
+  },
+};
