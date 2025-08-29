b01e941d16d3f4d93e1eb25b7276e7b8c353ce0a
Alena Petrych <abelokonskaya.87@gmail.com>
2025-08-29 10:02:39 +0100

    feat: added import/export feature to catalog js logic

--- scripts/catalogModul.js
+++ scripts/catalogModul.js
@@ -1,6 +1,9 @@
+import { watchDeatils } from "./catalogWatchSpecs";
+
 document.querySelectorAll(".view-details").forEach((bnt, index) => {
   bnt.addEventListener("click", () => {
     const item = bnt.closest(".catalog-item");
+    const id = item.dataset.id;
     const title = item.querySelector("h3").innerText;
     const price = item.querySelector("p").innerText;
     const imgSrc = item.querySelector("img").src;
@@ -9,11 +12,9 @@ document.querySelectorAll(".view-details").forEach((bnt, index) => {
     document.getElementById("modalPrice").innerText = price;
     document.getElementById("modalImage").src = imgSrc;
 
-    const specs = `
-<li>Category: ${item.dataset.category}</li>
-<li>Popularity: ${item.dataset.popular}</li>
-<li>Date: ${item.dataset.date}</li>
-`;
-    document.getElementById("modalSpecs").innerHTML = specs;
+    const specs = watchDetails[id]?.specs || ["No details available"];
+    document.getElementById("modalSpecs").innerHTML = specs
+      .map((s) => `<li>${s}</li>`)
+      .join("");
   });
 });
